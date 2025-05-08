b54b4a53b8c4873becc0c5503961f760e40e0f42
Hoon <ehdgns730@gmail.com>
2025-05-08 14:56:44 +0900

    250508

---
+++ 250508/js/default-named.js
@@ -0,0 +1,5 @@
+export default function multiply(a, b) {
+  return a * b;
+}
+
+export const pi = 3.14;
---
+++ 250508/js/default.js
@@ -0,0 +1,9 @@
+//두 수를 입력받아서 함수입력
+// export default function plus(x, y) {
+//     return x + y;
+// }
+
+//export default인 경우는 함수이름의 영향을 받지 않음음
+export default function(x, y) {
+    return x + y;
+}
